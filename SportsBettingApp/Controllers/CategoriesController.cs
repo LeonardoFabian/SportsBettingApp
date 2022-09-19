851ca0b185e0c35bc28a22f2ac2a5e309dd78a41
Leonardo Fabian <ramonlfabian@gmail.com>
2022-09-18 22:54:27 -0400

    Categories model and create category form

---
+++ SportsBettingApp/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsBettingApp.Models;
+
+namespace SportsBettingApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Categories category)
+        {
+            if( !ModelState.IsValid)
+            {
+                return View(category);
+            }
+            return View();
+        }
+    }
+}
---
+++ SportsBettingApp/Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsBettingApp.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "The length of the {0} field must be between {2} and {1}")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+    }
+}
