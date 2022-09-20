6fe8b48012c947449cb79da989728dd31598a4a7
Leonardo Fabian <ramonlfabian@gmail.com>
2022-09-19 22:40:57 -0400

    Create Users Service

--- SportsBettingApp/Program.cs
+++ SportsBettingApp/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<ICategoriesRepository, CategoriesRepository>();
+builder.Services.AddTransient<IUsersService, UsersService>();
 
 var app = builder.Build();
 
